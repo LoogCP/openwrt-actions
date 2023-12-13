7aab4120d2df85b382bf44df14b5ec0838614e9d
LoogCP <lp18883370076@gmail.com>
2023-12-13 16:51:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,9 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+
+#添加design主题
+git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
